804742f0b5dc0878bd6a7e709e14286372e1a4bc
Eds-Dbug <edsdbug@gmail.com>
2022-09-06 13:31:36 +0000

    add daylist item

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,7 +1,22 @@
 import React from "react";
-
 import "components/Button.scss";
+import classNames from "classnames";
 
 export default function Button(props) {
-   return <></>;
+
+  const confirm = props.confirm;
+  const danger = props.danger;
+  const disabled = props.disabled;
+  const handleClick = props.onClick;
+
+  let buttonClass = classNames('button', {"button--confirm": confirm,"button--danger": danger});
+
+  return (
+  <button 
+    className={buttonClass} 
+    onClick={handleClick} 
+    disabled={disabled}
+  >
+    {props.children}
+  </button>);
 }
